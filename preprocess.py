180d11424304d2df5f663aaaee09888467d56cb9
Jaden Wang <jadenyjw@gmail.com>
2017-09-16 07:26:19 -0400

    Preprocessing

--- preprocess.py
+++ preprocess.py
@@ -32,10 +32,8 @@ if shuffle_data:
 # Divide the hata into 60% train, 20% validation, and 20% test
 train_addrs = addrs[0:int(0.8*len(addrs))]
 train_labels = labels[0:int(0.8*len(labels))]
-val_addrs = addrs[int(0.8*len(addrs)):int(0.9*len(addrs))]
-val_labels = labels[int(0.8*len(addrs)):int(0.9*len(addrs))]
-test_addrs = addrs[int(0.9*len(addrs)):]
-test_labels = labels[int(0.9*len(labels)):]
+val_addrs = addrs[int(0.8*len(addrs)):]
+val_labels = labels[int(0.8*len(labels)):]
 
 img_dtype = tables.UInt8Atom()
 data_shape = (0, 224, 224, 3)
@@ -43,12 +41,10 @@ hdf5_file = tables.open_file(hdf5_path, mode='w')
 
 train_storage = hdf5_file.create_earray(hdf5_file.root, 'train_img', img_dtype, shape=data_shape)
 val_storage = hdf5_file.create_earray(hdf5_file.root, 'val_img', img_dtype, shape=data_shape)
-test_storage = hdf5_file.create_earray(hdf5_file.root, 'test_img', img_dtype, shape=data_shape)
 
 
 hdf5_file.create_array(hdf5_file.root, 'train_labels', train_labels)
 hdf5_file.create_array(hdf5_file.root, 'val_labels', val_labels)
-hdf5_file.create_array(hdf5_file.root, 'test_labels', test_labels)
 
 # a numpy array to save the mean of the images
 mean = np.zeros(data_shape[1:], np.float32)
@@ -86,19 +82,13 @@ for i in range(len(val_addrs)):
     # save the image
     val_storage.append(img[None])
 # loop over test addresses
-for i in range(len(test_addrs)):
     # print how many images are saved every 1000 images
 
     # read an image and resize to (224, 224)
     # cv2 load images as BGR, convert it to RGB
-    addr = test_addrs[i]
-    img = cv2.imread(addr)
-    img = cv2.resize(img, (224, 224), interpolation=cv2.INTER_CUBIC)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     # add any image pre-processing here
     # if the data order is Theano, axis orders should change
 
     # save the image
-    test_storage.append(img[None])
 # save the mean and close the hdf5 file
 hdf5_file.close()
